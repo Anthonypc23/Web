d269e1f741261a3bb4a62b8170b99e6e83a3382f
Anthony Richard Santos da Costa <65256753+Anthonypc23@users.noreply.github.com>
2022-05-18 21:31:25 -0300

    ,

--- fundamentos/exercicios-js/fundamentos/notacaoPonto.js
+++ fundamentos/exercicios-js/fundamentos/notacaoPonto.js
@@ -5,7 +5,7 @@ obj1.nome = 'bola'
 
 // obj1['nome'] = 'bola2'
 console.log(obj1.nome);
-// this instancia função a partir do atributo
+// this instancia função a partir do atributo em que ele esta associado
 function Obj (nome) {
     this.nome = nome
     this.exec = function(){
--- fundamentos/exercicios-js/fundamentos/objeto2.js
+++ fundamentos/exercicios-js/fundamentos/objeto2.js
@@ -3,8 +3,8 @@ console.log(typeof new Object());
 
 const cliente = function (){}
 
-console.log(typeof cliente);
-console.log(typeof new cliente)
+console.log(typeof cliente, '1');
+console.log(typeof new cliente, '2')
 
 class Produto {}//
 console.log(typeof Produto);
